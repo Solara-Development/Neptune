8666ee438b3ed269cdc1896d9fe0f8aab0fc2dfa
leoaxo098 <leoffpro098@gmail.com>
2026-06-15 19:57:09 +0700

    Revert "fix: resolve IllegalArgumentException in showHealth() by checking objective by name instead of display slot"
    This reverts commit 201397825416c80fc8150139b1c851bf2640af99.

--- Plugin/src/main/java/dev/lrxh/neptune/game/match/Match.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/match/Match.java
@@ -396,7 +396,7 @@ public abstract class Match implements IMatch {
 
     private void showHealth() {
         forEachPlayer(player -> {
-            Objective objective = player.getScoreboard().getObjective("neptune_health");
+            Objective objective = player.getScoreboard().getObjective(DisplaySlot.BELOW_NAME);
 
             if (objective == null) {
                 objective = player.getScoreboard().registerNewObjective("neptune_health", Criteria.HEALTH,
