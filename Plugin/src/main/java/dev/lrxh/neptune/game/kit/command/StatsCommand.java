99d22d297f4b2816d81adc5ef1915ab5c83559db
Devlrxxh <seabearvalorant@gmail.com>
2026-06-19 17:25:30 +0200

    Fixed /stats command always opening sender stats instead of target player

--- Plugin/src/main/java/dev/lrxh/neptune/game/kit/command/StatsCommand.java
+++ Plugin/src/main/java/dev/lrxh/neptune/game/kit/command/StatsCommand.java
@@ -2,19 +2,18 @@ package dev.lrxh.neptune.game.kit.command;
 
 
 import com.jonahseguin.drink.annotation.Command;
+import com.jonahseguin.drink.annotation.OptArg;
 import com.jonahseguin.drink.annotation.Sender;
 import dev.lrxh.neptune.game.kit.menu.StatsMenu;
 import org.bukkit.entity.Player;
 
 public class StatsCommand {
 
-    @Command(name = "", desc = "")
-    public void open(@Sender Player player) {
-        new StatsMenu(player.getName()).open(player);
-    }
-
-    @Command(name = "", desc = "", usage = "<player>")
-    public void statsOthers(@Sender Player player, Player target) {
+    @Command(name = "", desc = "", usage = "[player]")
+    public void stats(@Sender Player player, @OptArg Player target) {
+        if (target == null) {
+            target = player;
+        }
         new StatsMenu(target).open(player);
     }
 }
