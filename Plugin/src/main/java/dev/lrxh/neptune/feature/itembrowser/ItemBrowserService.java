a00edbf9ccc4a75ba720a741a8f2f695d2dfc88c
drappula <drappula@proton.me>
2026-09-22 20:57:58 +0530

    Remove the use of Material.values() which caused Bukkit Legacy Material Support to trigger (#200)
    * Removes the use of Material.values() which caused Bukkit Legacy Material Support to trigger
    
    Fixes #198
    
    * remove stream().toList()
    
    * Remove isLegacy() check
    
    ---------
    
    Co-authored-by: mallusrgreat <mallusrgreat@duck.com>

--- Plugin/src/main/java/dev/lrxh/neptune/feature/itembrowser/ItemBrowserService.java
+++ Plugin/src/main/java/dev/lrxh/neptune/feature/itembrowser/ItemBrowserService.java
@@ -87,16 +87,16 @@ public class ItemBrowserService implements IItemBrowserService {
 
     private List<Material> bySuffix(String suffix) {
         List<Material> list = new ArrayList<>();
-        for (Material m : Material.values()) {
-            if (m.isItem() && !m.isLegacy() && m.name().endsWith(suffix)) list.add(m);
+        for (Material m : Registry.MATERIAL) {
+            if (m.isItem() && m.name().endsWith(suffix)) list.add(m);
         }
         return list;
     }
 
     public List<Material> getAllItems() {
         List<Material> list = new ArrayList<>();
-        for (Material m : Material.values()) {
-            if (m.isItem() && !m.isAir() && !m.isLegacy()) list.add(m);
+        for (Material m : Registry.MATERIAL) {
+            if (m.isItem() && !m.isAir()) list.add(m);
         }
         return list;
     }
